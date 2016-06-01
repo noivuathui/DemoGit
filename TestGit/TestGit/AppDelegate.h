0cac590443e4fba1cb62626ff1216119e9f051c3
Ta Hoang Minh <tahoangminh269@gmail.com>
2016-06-01 20:31:23 +0700

    add source

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by Ta Hoang Minh on 6/1/16.
+//  Copyright © 2016 TechKid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Ta Hoang Minh on 6/1/16.
+//  Copyright © 2016 TechKid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
