28feb01f81667b00e256e049c3810c083c9c9e4c
Ta Hoang Minh <tahoangminh269@gmail.com>
2016-06-01 20:33:31 +0700

    update 2

--- TestGit/TestGit/ViewController.m
+++ TestGit/TestGit/ViewController.m
@@ -17,11 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"asldkas fa sdflasmlkfmaslkfasf alksf;laskf ;lakf;la sfd");
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:!animated];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    NSLog(@"asldkas fa sdflasmlkfmaslkfasf alksf;laskf ;lakf;la sfd");
 }
 
 @end
